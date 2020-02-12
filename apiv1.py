81ec7d2aa6f0fe2aff3d7981894a21a93ff95451
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-02-11 23:12:46 -0800

    add stripe routes

--- apiv1.py
+++ apiv1.py
@@ -5,7 +5,7 @@ from app_routes.namespace_academy import api as ns1
 from app_routes.namespace_challenges import api as ns2
 from app_routes.namespace_applications import api as ns3
 from app_routes.namespace_kubernetes import api as ns4
-# from app_routes.namespace_airflow import api as ns5
+from app_routes.namespace_stripe import api as ns5
 
 blueprint = Blueprint('apiv1', __name__)
 
@@ -14,6 +14,6 @@ api.add_namespace(ns1)
 api.add_namespace(ns2)
 api.add_namespace(ns3)
 api.add_namespace(ns4)
-# api.add_namespace(ns5)
+api.add_namespace(ns5)
 
 # REST
---
+++ app_routes/namespace_stripe.py
@@ -0,0 +1,60 @@
+from flask_restplus import Namespace, Resource, fields, reqparse, request
+from flask import jsonify
+from .initialize.academy import main
+from app_controllers.firestore.firestore_academy import FirestoreAcademy
+import json
+import os
+import stripe
+
+api = Namespace('stripe', description='Stripe related operations')
+
+stripe.api_key = os.getenv('STRIPE_SECRET_KEY')
+stripe.api_version = os.getenv('STRIPE_API_VERSION')
+
+@api.route('/public-key')
+class StripePublicKey(Resource):
+    @api.doc('get_public_key')
+    def get(self):
+        return jsonify(publicKey=os.getenv('STRIPE_PUBLISHABLE_KEY')), 200, {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "GET"} 
+
+@api.route('/create-customer')
+class StripeCreateCustomer(Resource):
+    @api.doc('create_customer')
+    def post(self):
+        data = json.loads(request.data)
+        paymentMethod = data['payment_method']
+        planIds = data['plan_ids']
+
+        # This creates a new Customer and attaches the PaymentMethod in one API call.
+        # At this point, associate the ID of the Customer object with your
+        # own internal representation of a customer, if you have one.
+        customer = stripe.Customer.create(
+            payment_method=paymentMethod,
+            email=data['email'],
+            invoice_settings={
+                'default_payment_method': paymentMethod
+            }
+        )
+
+        # In this example, we apply the coupon if the number of plans purchased
+        # meets or exceeds the threshold.
+        eligibleForDiscount = len(planIds) >= MIN_PLANS_FOR_DISCOUNT
+        coupon = os.getenv('COUPON_ID') if eligibleForDiscount else None
+
+        # Subscribe the user to the subscription created
+        subscription = stripe.Subscription.create(
+            customer=customer.id,
+            items=[{"plan": planId} for planId in planIds],
+            expand=["latest_invoice.payment_intent"],
+            coupon=coupon
+        )
+        return jsonify(subscription), 200, {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
+
+@api.route('/subscription')
+class StripeGetSubscription(Resource):
+    @api.doc('get_subscription')
+    def post(self):
+        # Reads application/json and returns a response
+        data = json.loads(request.data)
+        subscription = stripe.Subscription.retrieve(data['subscriptionId'])
+        return jsonify(subscription), 200, {'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Methods": "POST"} 
