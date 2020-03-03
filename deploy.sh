77094246159465045fcb010eeda819b49c602595
charles 🔥 <charleschong@Charless-MacBook-Pro.local>
2020-03-02 19:45:39 -0800

    ci(travis.yml): adjusted testing order
    instead of pytest selecting random tests alphabetically, run in specific order

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,14 @@
-pytest tests/test_travis_controller.py
-pytest tests/test_cloudrun_controller.py
-pytest tests/test_kubernetes_controller.py
+pytest -vs tests/app_schema/
+pytest -vs tests/app_controllers/test_travis_controller.py
+pytest -vs tests/app_controllers/test_clouddns_controller.py
+pytest -vs tests/app_controllers/test_cloudrun_controller.py
+pytest -vs tests/app_controllers/test_kubernetes_controller.py
 
 if [ $? -eq 0 ]; then
     git add .
     cz commit
     git push
 else
-    echo FAIL
+    echo FAILED
 fi
 
